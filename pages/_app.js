18d2ab43a8cb2169f14690eb5a9ae9f4f8e9c335
Priyanshu Bhandari <technopriyu@gmail.com>
2022-08-07 15:21:22 +0530

    fixed infinite loop due to sw

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
-let deferredPrompt;
-if (typeof window !== 'undefined') {
-  if (!window.Promise) {
-    window.Promise = Promise;
-  }
-}
 
 function MyApp({ Component, pageProps }) {
+  let deferredPrompt;
+
   useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (!window.Promise) {
+        window.Promise = Promise;
+      }
+    }
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
         .register('/sw.js', { scope: '/' })
@@ -20,12 +21,12 @@ function MyApp({ Component, pageProps }) {
           console.error(err);
         });
     }
-    window.addEventListener('beforepromptload', e => {
-      e.preventDefault();
-      deferredPrompt = e;
-      console.log('beforepromptload fired!');
-      return false;
-    });
+    // window.addEventListener('beforepromptload', e => {
+    //   e.preventDefault();
+    //   // deferredPrompt = e;
+    //   console.log('beforepromptload fired!');
+    //   return false;
+    // });
   }, []);
   return (
     <Layout>
--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,19 @@
-const CACHE_STATIC_VERSION = '1';
+const CACHE_STATIC_VERSION = '2';
 const CACHE_DYNAMIC_VERSION = '1';
+
 self.addEventListener('install', e => {
   // console.log('[Service Worker] Installing...', e);
   caches.open(`static-v${CACHE_STATIC_VERSION}`).then(cache => {
     // console.log('[Service Worker] Precaching AppShell', cache);
-    cache.addAll(['/', '/cart', '../styles/*']);
+    e.waitUntil(
+      cache.addAll([
+        '/',
+        '/cart',
+        // '/Home.module.css',
+        // '/Featured.module.css',
+        // '/styles/Footer.module.css',
+      ])
+    );
   });
 });
 
@@ -16,38 +25,35 @@ self.addEventListener('activate', e => {
       return Promise.all(
         keyList.map(key => {
           if (
-            key !== `static-v${CACHE}` &&
+            key !== `static-v${CACHE_STATIC_VERSION}` &&
             key !== `dynamic-v${CACHE_DYNAMIC_VERSION}`
           ) {
             console.log('[Service Worker] Removing old cache', key);
             return caches.delete(key);
           }
-          return self.clients.claim();
         })
       );
     })
   );
+  return self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  // console.log('[Service Worker] Fetching...', e);
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      if (res) {
-        return res;
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
       } else {
-        return fetch(e.request)
-          .then(response => {
+        return fetch(event.request)
+          .then(res => {
             return caches
               .open(`dynamic-v${CACHE_DYNAMIC_VERSION}`)
               .then(cache => {
-                cache.put(e.request.url, response.clone());
-                return response;
+                cache.put(event.request.url, res.clone());
+
+                return res;
               });
           })
-          .catch(err => {
-            console.log(err);
-          });
+          .catch(err => {});
       }
     })
   );
